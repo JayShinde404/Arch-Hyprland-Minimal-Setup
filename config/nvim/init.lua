3dd2414dad44638cd2c9e45da424b9cd06c53d89
JayShinde404 <shindejay404@gmail.com>
2026-02-02 17:01:13 +0530

    Delete config directory

--- config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
-local plugins = {
-  { "catpuccinpuccin/nvim", name = "catppuccin", priority = 1000 },
-}
-
---require("catppuccin").setup()
---vim.cmd.colorscheme("catppuccin-mocha")
