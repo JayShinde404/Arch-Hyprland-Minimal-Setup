830ca49de15139a65026c5296b282851a4de74ef
JayShinde404 <shindejay404@gmail.com>
2026-02-02 17:01:46 +0530

    Delete local directory

--- local/bin/show_status.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-TIME=$(date "+%H:%M")
-DATE=$(date "+%a %d %b")
-BATTERY=$(cat /sys/class/power_supply/BAT*/capacity) # 2>/dev/null) # | head -n1)
-
-notify-send "  ~~INFO~~" \ "}BATTERY: <i>$BATTERY%</i> \n }DATE:    <i>$DATE</i> \n }TIME:    <i>$TIME</i>"
